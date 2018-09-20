96cfd649553c8f234634968683a5c93aba8ed495
Erin Dubuc <erin.dubuc9@gmail.com>
2018-09-20 17:44:20 +0000

    added drop to delete collections

--- test/connection.js
+++ test/connection.js
@@ -7,12 +7,22 @@ mongoose.Promise = global.Promise;
 // create a before hook
 before(function(done){
     // connect to mongoDB
-    mongoose.connect('mongodb://localhost/testaroo');
+    mongoose.connect('mongodb://localhost/testaroo', { useNewUrlParser: true });
+    
     mongoose.connection.once('open', function(){
         console.log('Connection has been made, now make fireworks...');
         done();
     }).on('error', function(error){
-        console.log('Connection error:',error);
+        console.log('Connection error: ',error);
+    });
+});
+
+// before every test - empty the database so every test can run in isolation
+// Drop the characters collection before each test (drop = delete)
+beforeEach(function(done){
+    // drop the collection
+    mongoose.connection.collections.mariochars.drop(function(){
+        done();
     });
 });
 
